59199d138c22ded65c338bda66f1ea13308f2b87
tanisha ! <tanishagandhi2006@gmail.com>
2026-02-19 19:46:18 -0500

    Edited app.js

--- nemulatorApp/src/App.js
+++ nemulatorApp/src/App.js
@@ -6,11 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="box">
-          <h1 style={{wordSpacing: '100px'}}>
-            Name: 
-            Class
-            Level
-          </h1>
+          <p>
+          Name: <br/> 
+          Class: <br/>
+          Level:</p>
         </div>
       </header>
     </div>
